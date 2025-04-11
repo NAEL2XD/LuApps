81d3d6bb870fc11ad418128c7d100399743e2e07
Lunar <144155299+SyncGit12@users.noreply.github.com>
2025-04-10 21:01:22 -0300

    startup

--- source/Main.hx
+++ source/Main.hx
@@ -4,11 +4,24 @@ import debug.CrashHandler;
 import debug.FPSCounter;
 import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.FlxState;
 import lime.app.Application;
 import openfl.display.Sprite;
 import utils.Prefs;
 
-class Main extends Sprite {
+#if desktop
+import backend.ALSoftConfig; // Just to make sure DCE doesn't remove this, since it's not directly referenced anywhere else.
+#end
+class Main extends Sprite
+{
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var initialState:Class<FlxState> = state.StartupState; // default state it starts up in
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var framerate:Int = 60; // How many frames per second the game should run at.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+
 	// Main Settings
 	public static var luversion:String = "0.0.2"; // fart version
 	public static var sentenceCrash:Array<String> = [
@@ -24,11 +37,12 @@ class Main extends Sprite {
 
 	public static var fpsVar:FPSCounter;
 
-	public function new() {
+	public function new()
+	{
 		super();
 		CrashHandler.init();
 
-		addChild(new FlxGame(0, 0, state.PlayState, 120, 120, true));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPSCounter(3, 698, 0x00FFFFFF);
 		addChild(fpsVar);
@@ -38,5 +52,6 @@ class Main extends Sprite {
 		Prefs.loadPrefs();
 	}
 
-	public static function changeWindowName(name:String = "") Application.current.window.title = 'LuApps v$luversion ${name != "" ? '- $name' : ''}';
+	public static function changeWindowName(name:String = "")
+		Application.current.window.title = 'LuApps v$luversion ${name != "" ? '- $name' : ''}';
 }
--- source/debug/CrashHandler.hx
+++ source/debug/CrashHandler.hx
@@ -1,49 +1,54 @@
 package debug;
 
 // an fully working crash handler on ALL platforms
-
-import state.DummyState.Dummy;
-import flixel.tweens.FlxTween;
-import flixel.FlxSprite;
-import flixel.util.FlxColor;
-import flixel.text.FlxText;
-import flixel.FlxState;
 import flixel.FlxG;
-import openfl.events.UncaughtErrorEvent;
-import openfl.events.ErrorEvent;
-import openfl.errors.Error;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 import haxe.CallStack;
 import haxe.io.Path;
+import openfl.Lib;
+import openfl.errors.Error;
+import openfl.events.ErrorEvent;
+import openfl.events.UncaughtErrorEvent;
+import state.DummyState.Dummy;
 import sys.FileSystem;
 import sys.io.File;
-import openfl.Lib;
 
 using StringTools;
 
 /**
  * Crash Handler.
  * @author YoshiCrafter29, Ne_Eo. MAJigsaw77 and mcagabe19
-*/
-
-class CrashHandler {
+ */
+class CrashHandler
+{
 	public static var errorMessage:String = "";
 
-	public static function init():Void {
+	public static function init():Void
+	{
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
 		untyped __global__.__hxcpp_set_critical_error_handler(onError);
 	}
 
-	private static function onUncaughtError(e:UncaughtErrorEvent):Void {
-		try {
+	private static function onUncaughtError(e:UncaughtErrorEvent):Void
+	{
+		try
+		{
 			e.preventDefault();
 			e.stopPropagation();
 			e.stopImmediatePropagation();
-	
+
 			var m:String = e.error;
-			if (Std.isOfType(e.error, Error)) {
+			if (Std.isOfType(e.error, Error))
+			{
 				var err = cast(e.error, Error);
 				m = '${err.message}';
-			} else if (Std.isOfType(e.error, ErrorEvent)) {
+			}
+			else if (Std.isOfType(e.error, ErrorEvent))
+			{
 				var err = cast(e.error, ErrorEvent);
 				m = '${err.text}';
 			}
@@ -55,45 +60,62 @@ class CrashHandler {
 			var dateNow:String = Date.now().toString();
 			dateNow = dateNow.replace(" ", "_");
 			dateNow = dateNow.replace(":", "'");
-	
+
 			path = "crash/" + "LuApps_" + dateNow + ".log";
-	
-			for(stackItem in stack) {
-				switch(stackItem) {
-					case CFunction: stackLabelArr.push("Non-Haxe (C) Function");
-					case Module(c): stackLabelArr.push('Module ${c}');
+
+			for (stackItem in stack)
+			{
+				switch (stackItem)
+				{
+					case CFunction:
+						stackLabelArr.push("Non-Haxe (C) Function");
+					case Module(c):
+						stackLabelArr.push('Module ${c}');
 					case FilePos(parent, file, line, col):
-						switch(parent) {
+						switch (parent)
+						{
 							case Method(cla, func): stackLabelArr.push('${file.replace('.hx', '')}.$func() [line $line]');
 							case _: stackLabelArr.push('${file.replace('.hx', '')} [line $line]');
 						}
-					case LocalFunction(v): stackLabelArr.push('Local Function ${v}');
-					case Method(cl, m): stackLabelArr.push('${cl} - ${m}');
+					case LocalFunction(v):
+						stackLabelArr.push('Local Function ${v}');
+					case Method(cl, m):
+						stackLabelArr.push('${cl} - ${m}');
 				}
 			}
 			stackLabel = stackLabelArr.join('\r\n');
-	
+
 			errorMessage += 'Uncaught Error: $m\n$stackLabel';
 			trace(errorMessage);
-	
-			try {
-				if (!FileSystem.exists("crash/")) FileSystem.createDirectory("crash/");
+
+			try
+			{
+				if (!FileSystem.exists("crash/"))
+					FileSystem.createDirectory("crash/");
 				File.saveContent(path, errorMessage + "\n");
-			} catch(e) trace('Couldn\'t save error message. (${e.message})');
-	
+			}
+			catch (e)
+				trace('Couldn\'t save error message. (${e.message})');
+
 			Sys.println(errorMessage);
 			Sys.println("Crash dump saved in " + Path.normalize(path));
-		} catch(e:Dynamic) trace(e);
+		}
+		catch (e:Dynamic)
+			trace(e);
 
 		FlxG.switchState(Crashie.new);
 	}
 
-    private static function onError(message:Dynamic):Void throw Std.string(message);
+	private static function onError(message:Dynamic):Void
+		throw Std.string(message);
 }
 
-class Crashie extends FlxState {
-	override public function create() {
-		if (FlxG.sound.music != null) FlxG.sound.music.stop();
+class Crashie extends FlxState
+{
+	override public function create()
+	{
+		if (FlxG.sound.music != null)
+			FlxG.sound.music.stop();
 		Main.changeWindowName("Crash!");
 
 		var ohNo:FlxText = new FlxText(0, 0, 1280, 'LuApps ${Main.luversion} has crashed!');
@@ -103,7 +125,7 @@ class Crashie extends FlxState {
 		ohNo.y = 14;
 		add(ohNo);
 
-		var ohNo2:FlxText = new FlxText(0, 0, 1280, Main.sentenceCrash[FlxG.random.int(0, Main.sentenceCrash.length-1)]);
+		var ohNo2:FlxText = new FlxText(0, 0, 1280, Main.sentenceCrash[FlxG.random.int(0, Main.sentenceCrash.length - 1)]);
 		ohNo2.setFormat('assets/fonts/main.ttf', 18, FlxColor.WHITE, FlxTextAlign.CENTER);
 		ohNo2.alpha = 0;
 		ohNo2.screenCenter();
@@ -113,9 +135,10 @@ class Crashie extends FlxState {
 		CrashHandler.errorMessage += "\n\nIf that happens because of an LuApp error,\neither give details on what you did or\nContact the developer that made the LuApp application";
 		var stripClub:Array<String> = CrashHandler.errorMessage.split("\n");
 		var i:Int = -1;
-		var crash:Array<FlxText> =[];
+		var crash:Array<FlxText> = [];
 
-		for (line in stripClub) {
+		for (line in stripClub)
+		{
 			i++;
 			crash.push(new FlxText(180, 0, 1280, line));
 			crash[i].setFormat('assets/fonts/settings.ttf', 16, FlxColor.WHITE, FlxTextAlign.LEFT);
@@ -125,7 +148,7 @@ class Crashie extends FlxState {
 			crash[i].y = 120 + (16 * i);
 			add(crash[i]);
 		}
-		
+
 		var tip:FlxText = new FlxText(180, 0, 1280, "Press any key to restart. (Press ENTER to launch GitHub)");
 		tip.setFormat('assets/fonts/main.ttf', 36, FlxColor.WHITE, FlxTextAlign.CENTER);
 		tip.alpha = 0;
@@ -136,14 +159,18 @@ class Crashie extends FlxState {
 		FlxTween.tween(ohNo, {alpha: 1}, 0.5);
 		FlxTween.tween(ohNo2, {alpha: 1}, 0.5);
 		FlxTween.tween(tip, {alpha: 1}, 0.5);
-		for (spr in crash) FlxTween.tween(spr, {alpha: 1}, 0.5);
+		for (spr in crash)
+			FlxTween.tween(spr, {alpha: 1}, 0.5);
 
 		super.create();
 	}
 
-	override public function update(elapsed:Float) if (FlxG.keys.justPressed.ANY) {
-		if (FlxG.keys.justPressed.ENTER) FlxG.openURL("https://github.com/NAEL2XD/LuApps/issues");
-		CrashHandler.errorMessage = "";
-		Dummy.exit();
-	}
+	override public function update(elapsed:Float)
+		if (FlxG.keys.justPressed.ANY)
+		{
+			if (FlxG.keys.justPressed.ENTER)
+				FlxG.openURL("https://github.com/NAEL2XD/LuApps/issues");
+			CrashHandler.errorMessage = "";
+			Dummy.exit();
+		}
 }
